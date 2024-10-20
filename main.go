ae553e5ac334b12f3bf47ca04211ece74aa70e00
aihouRi <aihou971121@gmail.com>
2024-10-20 21:36:29 +0900

    a sample template

--- main.go
+++ main.go
@@ -1,52 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
-var portNumber = ":8080"
+const portNumber = ":8080"
 
 // Home is a home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the home page")
+	renderTemplate(w, "home.page.tmpl")
 }
 
 // About is a about page handler
 func About(w http.ResponseWriter, r *http.Request) {
-	sum := addVaules(2, 2)
-	fmt.Fprintf(w, "This is the about page and 2 + 2 is %d", sum)
+	
 }
 
-func addVaules(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideVaules(100.0, 10.0)
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
-		fmt.Fprintf(w, "can not divide by 0")
+		fmt.Println("error parsing template", err)
 		return
 	}
-	fmt.Fprintf(w, "%f divide by %f is %f", 100.0, 10.0, f)
-}
-
-func divideVaules(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("can not divide by zeor")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
 }
 
 func main() {
 
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
-
 
 	fmt.Printf("Starting app on port %s\n", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
