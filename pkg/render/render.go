afe7a64879c29c407b0d92347799b406a72380e2
aihouRi <aihou971121@gmail.com>
2024-10-23 14:48:30 +0900

    Optimized the create function code

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -34,37 +34,45 @@ func RenderTemplate(w http.ResponseWriter, tmpl string) {
 	}
 }
 
+
+//以下是发现问题后修改的，更新github后本地会删除它（2024年10月23日）
 func createTemplateCache() (map[string]*template.Template, error) {
-	//myCache := make(map[string]*template.Template)
 	myCache := map[string]*template.Template{}
 
-	//get all if the fikes named *.tmpl from ./templates
+	// 获取所有页面模板文件
 	pages, err := filepath.Glob("./templates/*.page.tmpl")
 	if err != nil {
 		return myCache, err
 	}
 
-	//range through all files ending with *.page.tmpl
+	// 获取所有布局模板文件，只需要查找一次
+	layoutFiles, err := filepath.Glob("./templates/*.layout.tmpl")
+	if err != nil {
+		return myCache, err
+	}
+
+	// 遍历每个页面模板
 	for _, page := range pages {
 		name := filepath.Base(page)
+
+		// 解析页面模板
 		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
 			return myCache, err
 		}
-		matches, err := filepath.Glob("./templates/*.page.tmpl")
-		if err != nil {
-			return myCache, err
-		}
 
-		if len(matches) > 0 {
-			ts, err = ts.ParseGlob("./templates/*.layout.tmpl")
+		// 如果有布局模板，解析布局模板
+		if len(layoutFiles) > 0 {
+			ts, err = ts.ParseFiles(layoutFiles...)
 			if err != nil {
 				return myCache, err
 			}
 		}
 
+		// 将解析后的模板存入缓存
 		myCache[name] = ts
 	}
 
-	return myCache, err
+	return myCache, nil
 }
+
